510194beb5c55cd12fc01a3a8ba40268964ab7d7
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-05-09 13:01:51 +0300

    Linking the simprints ID

--- App.tsx
+++ App.tsx
@@ -1,26 +1,55 @@
 import React, {useCallback} from 'react';
-import {Alert, Button, Linking, StyleSheet, View} from 'react-native';
+import {Alert, Button, Linking, StyleSheet, View, Platform} from 'react-native';
 
-const SendIntentButton = ({action, extras, children}) => {
-  const handlePress = useCallback(async () => {
-    try {
-      await Linking.sendIntent(action, extras);
-    } catch (e) {
-      Alert.alert('An error has occurred');
-    }
-  }, [action, extras]);
+// const SendIntentButton = ({action, extras, children}) => {
+//   const handlePress = useCallback(async () => {
+//     try {
+//       await Linking.sendIntent(action, extras);
+//     } catch (e) {
+//       Alert.alert('An error has occurred');
+//     }
+//   }, [action, extras]);
 
-  return <Button title={children} onPress={handlePress} />;
-};
+//   return <Button title={children} onPress={handlePress} />;
+// };
+
+// const OpenSimprintsButton = () => {
+//   const handlePress = useCallback(async () => {
+//     try {
+//       await Linking.openURL(
+//         'https://www.simprints.com/id/',
+//       );
+//     } catch (e: any) {
+//       Alert.alert(e.message);
+//     }
+//   }, []);
+
+//   return <Button title="Open Simprints" onPress={handlePress} />;
+// };
 
 const OpenSimprintsButton = () => {
   const handlePress = useCallback(async () => {
+    const appPackageId = 'com.simprints.id';
+    const appUrl = `${appPackageId}://`;
+    const playStoreUrl = `https://play.google.com/store/apps/details?id=${appPackageId}`;
+    const simprintsUrl = 'https://www.simprints.com/id/';
+
+    let url = appUrl;
+    if (Platform.OS === 'android') {
+      const isAppInstalled = await Linking.canOpenURL(appUrl);
+      console.log(isAppInstalled);
+      if (!isAppInstalled) {
+        url = playStoreUrl;
+      }
+    }
+
     try {
-      await Linking.sendIntent('android.intent.action.MAIN', [
-        {key: 'package', value: 'com.simprints.id'},
-      ]);
-    } catch (e) {
-      Alert.alert('An error has occurred');
+      await Linking.openURL(url);
+      //await Linking.sendIntent(appPackageId);
+    } catch (e: any) {
+      Alert.alert(e.message);
+      // if opening the app fails, try opening the Simprints website as a fallback
+      //await Linking.openURL(simprintsUrl);
     }
   }, []);
 
@@ -30,14 +59,7 @@ const OpenSimprintsButton = () => {
 const App = () => {
   return (
     <View style={styles.container}>
-      <SendIntentButton  action="android.intent.action.POWER_USAGE_SUMMARY">
-        Power Usage Summary
-      </SendIntentButton>
-
-      <SendIntentButton action="android.intent.action.MAIN" extras={[{key: 'package', value: 'com.simprints.id'}]}>
-        Open Simprints ID
-      </SendIntentButton>
-
+      <OpenSimprintsButton />
     </View>
   );
 };
@@ -48,7 +70,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  
 });
 
 export default App;
